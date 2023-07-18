166226e1069c3e7f90118cf17fd6e029c11cadc8
sachin sherekar <sachinsherekar11@gmail.com>
2023-07-18 13:08:15 +0530

    submitting code-Tue Jul 18 2023 13:08:13 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import ButtonCounter from "./ButtonCounter";
+
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <ButtonCounter/>
   )
 }
 
---
+++ src/components/ButtonCounter.js
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+
+const ButtonCounter =()=>{
+    let [count,setCount] = useState(0);
+
+
+ 
+function increaseCount (){
+    setCount(count+1);
+}
+
+return(
+    <div>
+        <p>Button Clicked {count} times</p>
+        <button onClick={increaseCount}>Click me</button>
+
+    </div>
+)
+}
+export default ButtonCounter;
